79a4eee5a1c14733253ca4614d511f7ef13223ad
Randaleee <134199561+Randaleee@users.noreply.github.com>
2025-09-29 20:12:13 +0200

    zweiter push

---
+++ Data/Projekte/Apps.gd
@@ -0,0 +1,51 @@
+class_name App
+extends Resource
+
+@export_group("Spielerentscheidung")
+@export var plattform: Plattform
+@export var kategorie: Kategorie
+@export var entwicklungszeit: int
+@export var app_name: String
+@export var schwerpunkt: Schwerpunkt
+
+@export_group("Eigenschaften")
+@export_range(1, 5) var schwierigkeit: int # hängt von der Kategorie ab
+@export var gewährte_erfahrung: int
+@export var belohnung: int # Geldeinheit
+
+@export_group("Attribute")
+@export_range(1, 200) var design: int
+@export_range(1, 200) var performance: int
+@export_range(1, 200) var innovation: int
+@export_range(1, 200) var qualität: int
+
+enum Schwerpunkt {
+	DESIGN,
+	PERFORMANCE,
+	INNOVATION,
+	QUALITÄT,
+}
+
+enum Plattform {
+	NEWTON,
+	HUMANOID,
+	RAINFOREST,
+	DOORS,
+	SUNGSUM
+}
+
+enum Kategorie {
+	UNTERHALTUNG,
+	SOZIALES,
+	BILDUNG,
+	PRODUKTIVITÄT,
+	GESUNDHEIT,
+}
+
+const KATEGORIE_NAMEN = {
+	Kategorie.UNTERHALTUNG: "Unterhaltung",
+	Kategorie.SOZIALES: "Soziales",
+	Kategorie.BILDUNG: "Bildung",
+	Kategorie.PRODUKTIVITÄT: "Produktivität",
+	Kategorie.GESUNDHEIT: "Gesundheit",
+}
--- angestellte_mitarbeiter.gd
+++
@@ -1,9 +0,0 @@
-class_name Angestellte_Mitarbeiter
-
-var _content:Array[Mitarbeiter] = []
-
-func add_mitarbeiter(mitarbeiter:Mitarbeiter):
-	_content.append(mitarbeiter)
-
-func remove_mitarbeiter(mitarbeiter:Mitarbeiter):
-	_content.erase(mitarbeiter)
